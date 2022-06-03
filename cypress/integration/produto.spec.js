1438e30997e481b8658b99dcd1b5fb906ef758d4
jenifferdnz <jenifferdiniz02@gmail.com>
2022-06-03 12:10:56 -0300

    Correção atividade

--- cypress/integration/produto.spec.js
+++ cypress/integration/produto.spec.js
@@ -32,8 +32,8 @@ describe('Testes de Funcionalidade Produtos', () => {
             method:'POST',
             url: 'usuarios',
             body:{
-                "nome": 'Madia Eduarda',
-                "email": 'duda2@ebac.com',
+                "nome": 'Maria Letícia',
+                "email": 'duda44@ebac.com',
                 "password": "teste",
                 "administrador": "true"
             },
@@ -46,7 +46,7 @@ describe('Testes de Funcionalidade Produtos', () => {
         
     });
 
-    it.only('Validar um usuário com email inválido', () => {
+    it('Validar um usuário com email inválido', () => {
         cy.request({
             failOnStatusCode: false,
             method:'POST',
@@ -64,18 +64,18 @@ describe('Testes de Funcionalidade Produtos', () => {
     });
 
     it('Deve editar um usuário previamente cadastrado', () => {
-        cy.request('produtos').then(response => {
-            let id = response.body.produtos[24]._id
+        cy.request('usuarios').then(response => { 
+            var id = response.body.usuarios[1]._id
             cy.request({
                 method: 'PUT',
-                url: `produtos/${id}`,
+                url: `usuarios/${id}`,
                 headers: {authorization: token},
                 body:
                 {
-                    "nome": "PRODUTO 1000",
-                    "preco": 299,
-                    "descricao": "Novo produto",
-                    "quantidade": 109
+                    "nome": "Nome user Editado",
+                    "email": "duda44@ebac.com",
+                    "password": "teste",
+                    "administrador": "true"
 
                 }
             }).then(response =>{
@@ -85,45 +85,19 @@ describe('Testes de Funcionalidade Produtos', () => {
 
     });
 
-    it('Deve deletar um usuário previamente cadastrado', () => {
-        let produto = `Produto EBAC ${Math.floor(Math.random()* 10000000)}`
-        cy.cadastrarProduto(token, produto, "5000", "Curso", "200")
-        .then(response => {
-            let id = response.body._id
+    it.only('Deve deletar um usuário previamente cadastrado', () => {
 
+        cy.request('usuarios').then(response => {
+            var id = response.body.usuarios[1]._id
             cy.request({
-                method: 'PUT',
-                url: `produtos/${id}`,
-                headers: {authorization: token},
-                body:
-                {
-                    "nome": produto,
-                    "preco": 2022,
-                    "descricao": "Novo produto",
-                    "quantidade": 109
-
-                }
-            }).then(response =>{
-                expect(response.body.message).to.equal('Registro alterado com sucesso')
-            })
-        })
-
-
-    });
-
-    it('Deve deletar um produto previamente cadastrado', () => {
-        let produto = `Produto EBAC ${Math.floor(Math.random()* 10000000)}`
-        cy.cadastrarProduto(token, produto, "5000", "Curso", "200")
-        .then(response =>{
-            let id = response.body._id
-            cy.request({
-                method: 'DELETE',
-                url: `produtos/${id}`,
-                headers: {authorization: token}
-            }).then(response =>{
-                expect(response.body.message).to.equal('Registro excluído com sucesso')
+                method: 'DELETE', 
+                url: `usuarios/${id}`, 
+                
+            }).then(response => {
+                expect(response.body.message).to.equal("Registro excluído com sucesso")
                 expect(response.status).to.equal(200)
             })
-        })        
-    });
+        })
+    })
+        
 });
